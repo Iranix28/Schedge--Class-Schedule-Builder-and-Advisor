5a62f8ce27a659b2060fac665d62856a761d5f14
abdulahadasim <abdulahad.asim@gmail.com>
2025-11-16 15:55:30 -0700

    Fixed frontend not connecting with backend

--- backend/app/config/env_variables.py
+++ backend/app/config/env_variables.py
@@ -1,7 +1,7 @@
 #gotta change this to be safer, will do this at some point -abdul
 
-OLLAMA_URL = "http://localhost:11434/v1/chat/completions"
+OLLAMA_URL = "http://host.docker.internal:11434/v1/chat/completions"
 API_KEY = "local"          # whatever you use nowW
-MODEL = "llama3.2:1b"      # this can change whenever, just a test
+MODEL = "deepseek-r1:8b"      # this can change whenever, just a test
 
 DATABASE_URL = "postgresql+psycopg://postgres:postgres@host.docker.internal:5432/schedge_db"
