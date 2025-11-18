fc57bb5469d91a08d1d6ebfe4b704727512ee315
Valentin Motta de Castro <111603514+ValentinMDC@users.noreply.github.com>
2025-11-18 16:59:36 -0700

    Removed Poetry Content
    Can run ngrok straight through fastAPI rather than through poetry server.

--- backend/app/config/env_variables.py
+++ backend/app/config/env_variables.py
@@ -1,7 +1,7 @@
 #gotta change this to be safer, will do this at some point -abdul
 
-OLLAMA_URL = "https://unjudicial-sherilyn-ruly.ngrok-free.dev" #this link may change, good right now for development
+OLLAMA_URL = "http://host.docker.internal:11434/v1/chat/completions" #this link may change, good right now for development
 API_KEY = "local"          # whatever you use nowW
-MODEL = "deepseek-r1:1.5b"      # this can change whenever, just a test
+MODEL = "deepseek-r1:8b"      # this can change whenever, just a test
 
 DATABASE_URL = "postgresql+psycopg://postgres:postgres@host.docker.internal:5432/schedge_db"
--- frontEnd/ChatUI.js
+++ frontEnd/ChatUI.js
@@ -1,7 +1,7 @@
 const { useState, useRef, useEffect } = React;
 
 // DeepSeek-R1 (Ollama /v1 path) ---
-const BASE_URL = "http://localhost:8000/ollama"; // FastAPI, not Ollama
+const BASE_URL = "https://unjudicial-sherilyn-ruly.ngrok-free.dev/ollama"; // FastAPI, not Ollama
 // const API_KEY = "local"; // not needed for FastAPI unless you want it
 
 async function sendMessageLLM(userText, onToken) {
