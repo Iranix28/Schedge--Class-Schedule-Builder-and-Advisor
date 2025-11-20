a3935fe02b817320842ca49cfac08aaf2099ffe7
Iranix28 <133243559+Iranix28@users.noreply.github.com>
2025-11-19 21:14:42 -0700

    Implemented the audit router
    Made a router file for the audit that calls the audit scraper method

---
+++ backend/app/routers/audit_requirements_router.py
@@ -0,0 +1,22 @@
+from typing import List, Optional
+
+from fastapi import APIRouter, Depends, HTTPException, Query, UploadFile, File
+from sqlalchemy.orm import Session
+
+from app.database.session import DBSession
+from app.models.db_models import CoursePrerequisiteCreate, CoursePrerequisiteRead
+
+from app.database.query_routers.course_prerequisites_query import create_course_prerequisite, list_course_prerequisites, get_course_prerequisite
+
+from backend.exceptions import EntityNotFound
+
+from scraper.audit_scraper import scrapeDegreeAudit
+
+router = APIRouter(prefix="/upload-audit", tags=["audit-tag"])
+
+
+@router.post("/", status_code=201)
+def post_degree_audit(file: UploadFile = File(...)):
+    reqs = scrapeDegreeAudit(file.file)
+
+    return reqs
--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 #python -m uvicorn routers.main:app --reload --port 8000
 #run command above to run server
 
-from app.routers import ollama_router, departments_router, courses_router, class_sections_router, course_prerequisites_router
+from app.routers import ollama_router, departments_router, courses_router, class_sections_router, course_prerequisites_router, audit_requirements_router
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 from fastapi import Request
@@ -34,6 +34,7 @@ app.include_router(departments_router.router)
 app.include_router(courses_router.router)
 app.include_router(class_sections_router.router)
 app.include_router(course_prerequisites_router.router)
+app.include_router(audit_requirements_router.router)
 
 @app.get("/status", response_model=None, status_code=204)       #testing bruv
 def status():
