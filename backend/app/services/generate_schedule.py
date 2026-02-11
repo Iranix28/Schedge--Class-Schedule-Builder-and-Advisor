aaa4a2ed8515601932236ae0a0ef20557d3f47bb
Iranix28 <133243559+Iranix28@users.noreply.github.com>
2026-02-11 14:29:28 -0700

    Fixed duplicate database table bug

--- backend/app/services/generate_schedule.py
+++ backend/app/services/generate_schedule.py
@@ -1,13 +1,13 @@
-from backend.app.database.query_routers.audit_requirements_query import get_audit_tree, get_completed_courses, is_course_completed
-from backend.app.database.query_routers.course_prerequisites_query import get_course_prerequisites_by_subject
-from backend.app.database.query_routers.departments_query import get_department
-from backend.app.database.query_routers.courses_query import get_course_id
-from backend.app.database.query_routers.class_sections_query import get_class_sections_by_course_number
-from backend.scraper.audit_scraper import splitCourse
+from app.database.query_routers.audit_requirements_query import get_audit_tree, get_completed_courses, is_course_completed
+from app.database.query_routers.course_prerequisites_query import get_course_prerequisites_by_subject
+from app.database.query_routers.departments_query import get_department
+from app.database.query_routers.courses_query import get_course_id
+from app.database.query_routers.class_sections_query import get_class_sections_by_course_number
+from scraper.audit_scraper import splitCourse
 from app.database.session import SessionLocal
 from app.models.models import ScheduleItem
 from datetime import datetime
-from backend.app.database.schema import ClassSection
+from app.database.schema import ClassSection
 
 def convert_days(days, day_string):
     if "Mo" in day_string:
@@ -169,88 +169,4 @@ def generate_schedule(audit_id):
 
                         totalClasses -= 1
     
-    return schedule
-
-    # for requirement in audit.requirements:
-    #     if "Capstone" in requirement.title:
-
-    #         for subreq in requirement.subrequirements:
-
-    #             if "Project" in subreq.title:
-
-    #                 for course in subreq.select_from:
-
-    #                     if "4500" in course:
-    #                         print("FOUND CAPSTONE")
-    #                         print(course)
-    #                         add_class_to_schedule(db, course, schedule)
-    #                         for c in schedule:
-    #                             print(c.class_)
-
-    #     if "Computer Science Electives" in requirement.title:
-            
-    #         for subreq in requirement.subrequirements:
-    #             if "[No Title]" in subreq.title:
-    #                 rules = subreq.rules
-    #                 for rule in rules:
-    #                     if rule.kind == "RANGE":
-    #                         min_class = rule.number_min
-    #                         max_class = rule.number_max
-    #                         rule_subject = rule.subject
-    #                         courses = list_courses_in_number_range(db, rule_subject, min_class, max_class)
-    #                         for course in courses:
-
-
-    #                             if course.number == "5955" or course.number == "4530" or course.number == "3090":
-    #                                 add_class_to_schedule(db, rule_subject + " " + course.number, schedule)
-    #     #print(schedule)
-    # return schedule
-
-        # needs = 0
-        # credits = 0
-
-
-
-        # # If subrequirement is based on credits to take
-        # if subreq.needs_credits is not None:
-        #     for course in subreq.select_from:
-
-        #         # Only add courses that are not prohibited
-        #         if course not in subreq.not_from:
-        #             if credits >= subreq.needs_credits:
-        #                 break
-
-        #             dept, num = splitCourse(course)
-
-        #             possible_class = get_course_by_code(dept, num)
-
-        #             # If the class exists in the catalog
-        #             if possible_class is not None:
-        #                 # Add class to schedule
-
-        #                 credits += 1 # change to number of credits
-        #                 classes_num += 1
-                        
-        #     continue
-
-        # # If subrequirement is based on number of classes to take (needs count)
-        # if subreq.needs_count is not None:
-        #     for course in subreq.select_from:
-
-        #         # Only add courses that are not prohibited
-        #         if course not in subreq.not_from:
-        #             if needs >= subreq.needs_count:
-        #                 break
-
-        #             dept, num = splitCourse(course)
-
-        #             possible_class = get_course_by_code(dept, num)
-
-        #             # If the class exists in the catalog
-        #             if possible_class is not None:
-        #                 # Add class to schedule
-
-        #                 needs += 1
-        #                 classes_num += 1
-
     return schedule
